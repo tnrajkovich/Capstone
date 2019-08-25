3e90fee1aef34df0fc3cfd08a75932cf066591de
Tom Rajkovich <tnrajkovich@gmail.com>
2019-08-25 12:58:07 -0500

    Added user recipe mods to parent recipe show page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,3 @@
 class Category < ApplicationRecord
   has_many :parent_recipes
-  has_many :user_recipes, through: :parent_recipes
 end
--- app/models/parent_recipe.rb
+++ app/models/parent_recipe.rb
@@ -1,5 +1,4 @@
 class ParentRecipe < ApplicationRecord
   belongs_to :user
-  belongs_to :category
   has_many :user_recipes
 end
