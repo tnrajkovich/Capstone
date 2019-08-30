071dd2e44746b2459828b27afa4f602fbd7cc051
Tom Rajkovich <tnrajkovich@gmail.com>
2019-08-29 19:25:36 -0500

    Edited sessions controller and user_recipe json file to allow for showing of username

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -5,12 +5,11 @@ class Api::SessionsController < ApplicationController
       jwt = JWT.encode(
         {
           user_id: user.id,
-          exp: 24.hours.from_now.to_i,
         },
         Rails.application.credentials.fetch(:secret_key_base),
         "HS256"
       )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      render json: { jwt: jwt, email: user.email, user_id: user.id, username: user.username }, status: :created
     else
       render json: {}, status: :unauthorized
     end
