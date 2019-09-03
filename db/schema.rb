f26b69074a3ebdad840a76711e12e1811607503d
Tom Rajkovich <tnrajkovich@gmail.com>
2019-09-02 19:34:06 -0500

    Removed redundant votable gem

--- db/schema.rb
+++ db/schema.rb
@@ -54,21 +54,4 @@ ActiveRecord::Schema.define(version: 2019_08_30_011630) do
     t.string "password_digest"
     t.boolean "admin"
   end
-
-  create_table "votes", force: :cascade do |t|
-    t.string "votable_type"
-    t.bigint "votable_id"
-    t.string "voter_type"
-    t.bigint "voter_id"
-    t.boolean "vote_flag"
-    t.string "vote_scope"
-    t.integer "vote_weight"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
-    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
-    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
-    t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
-  end
-
 end
