693d5c352c5da2f4798253ea36a58c4cb4b7c040
Tom Rajkovich <tnrajkovich@gmail.com>
2019-09-04 20:33:45 -0500

    Fixed typo in users controller for error messages

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -13,7 +13,7 @@ class Api::UsersController < ApplicationController
     if @user.save
       render json: { message: "User created successfully" }, status: :created
     else
-      render json: { errors: user.errors.full_messages }, status: :bad_request
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
   end
 end
