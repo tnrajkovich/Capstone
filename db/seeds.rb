fd0f454963d1b039be638a5e515f809d74069a12
Tom Rajkovich <tnrajkovich@gmail.com>
2019-09-04 19:49:06 -0500

    Working on updating seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,162 @@
 
 User.create!(first_name: "Andrew", last_name: "Cardoza", username: "acardoza", email: "acardoza@email.com", password: "password")
 User.create!(first_name: "Nick", last_name: "Bette", username: "nbette", email: "nbette@email.com", password: "password")
+User.create!(first_name: "Tom", last_name: "Rajkovich", username: "trajkovich", email: "trajkovich@email.com", password: "password")
+User.create!(first_name: "Sarah", last_name: "Fleming", username: "sfleming", email: "sfleming@email.com", password: "password")
 
-ParentRecipe.create!(title: "Pasta carbonara", ingredients: "Pasta, eggs, parmesan, pancetta, black pepper", directions: "Make Carbonara", category_id: 1, user_id: 1)
-ParentRecipe.create!(title: "Pad Thai", ingredients: "Noodles, green onion, bean sprouts, peanuts, lime, chicken", directions: "Make Pad Thai!", category_id: 2, user_id: 1)
-ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 3, user_id: 2)
+ParentRecipe.create!(title: "Pasta carbonara", 
+  ingredients: "
+  Salt
+2  large eggs and 2 large yolks, room temperature
+1  ounce (about 1/3 packed cup) grated pecorino Romano, plus additional for serving
+1  ounce (about 1/3 packed cup) grated Parmesan
+ Coarsely ground black pepper
+1  tablespoon olive oil
+3 ½  ounces of slab guanciale (see recipe), pancetta or bacon, sliced into pieces about 1/4 inch thick by 1/3 inch square
+12  ounces spaghetti (about 3/4 box)", 
+directions: "
+Place a large pot of lightly salted water (no more than 1 tablespoon salt) over high heat, and bring to a boil. Fill a large bowl with hot water for serving, and set aside.
+In a mixing bowl, whisk together the eggs, yolks and pecorino and Parmesan. Season with a pinch of salt and generous black pepper.
+Set the water to boil. Meanwhile, heat oil in a large skillet over medium heat, add the pork, and sauté until the fat just renders, on the edge of crispness but not hard. Remove from heat and set aside.
+Add pasta to the water and boil until a bit firmer than al dente. Just before pasta is ready, reheat guanciale in skillet, if needed. Reserve 1 cup of pasta water, then drain pasta and add to the skillet over low heat. Stir for a minute or so.
+Empty serving bowl of hot water. Dry it and add hot pasta mixture. Stir in cheese mixture, adding some reserved pasta water if needed for creaminess. Serve immediately, dressing it with a bit of additional grated pecorino and pepper.", 
+category_id: 1, 
+user_id: 1, 
+video: "http://www.youtube.com/embed/MbRm5s3i2ik")
+ParentRecipe.create!(title: "Pad Thai", ingredients: "Kosher salt
+8 oz. wide rice noodles
+2 tbsp. lime juice
+2 tbsp. brown sugar
+1 tbsp. fish sauce
+1 tbsp. low-sodium soy sauce
+1/4 tsp. cayenne pepper
+2 tbsp. vegetable oil
+1 bell pepper, thinly sliced
+2 cloves garlic, minced
+2 eggs, lightly whisked
+1 lb. shrimp, peeled and deveined
+Freshly ground black pepper
+2 green onions, thinly sliced
+1/4 c. roasted peanuts, chopped", 
+directions: "In a large pot of salted boiling water, cook noodles until al dente. Drain.
+In a small bowl, whisk together lime juice, brown sugar, fish sauce, soy sauce, and cayenne pepper. Set aside.
+In a large nonstick pan over medium-high heat, heat oil. Add bell pepper and cook until tender, about 4 minutes. Stir in garlic and cook until fragrant, about 1 minute more. Add the shrimp and season with salt and pepper. Cook until pink, about 2 minutes per side.
+Push the shrimp and vegetables to one side of the pan and pour in the egg. Scramble until just set then mix with the shrimp mixture. Add the cooked noodles and toss until combined. Pour in the lime juice mixture and toss until the noodles are coated.
+Garnish with green onions and roasted peanuts before serving.", 
+category_id: 2, 
+user_id: 1, 
+video: "https://www.youtube.com/embed/DsyfYJ5Ou3g")
+ParentRecipe.create!(title: "Bahn Mi", ingredients: "½ cup rice vinegar
+¼ cup water
+¼ cup white sugar
+¼ cup carrot, cut into 1/16-inch-thick matchsticks
+¼ cup white (daikon) radish, cut into 1/16-inch-thick matchsticks
+¼ cup thinly sliced white onion
+1 skinless, boneless chicken breast half  
+garlic salt to taste
+ground black pepper to taste
+1 (12 inch) French baguette
+4 tablespoons mayonnaise
+¼ cup thinly sliced cucumber
+1 tablespoon fresh cilantro leaves
+1 small jalapeno pepper - seeded and cut into 1/16-inch-thick matchsticks
+1 wedge lime", directions: "
+Place rice vinegar, water, and sugar into a saucepan over medium heat, bring to a boil, and stir until the sugar has dissolved, about 1 minute. Allow the mixture to cool.
+
+Pour the cooled vinegar mixture over the carrot, radish, and onion in a bowl, and allow to stand for at least 30 minutes. Drain off the excess vinegar mixture after the vegetables have marinated.
+
+While the vegetables are marinating, preheat the oven's broiler, and set the oven rack about 6 inches from the heat source. Lightly oil a slotted broiler pan.
+
+Sprinkle the chicken breast with garlic salt and pepper, and broil on slotted broiler pan, turning once, until the center of the chicken breast is no longer pink and the surface has browned, about 6 minutes per side. Remove the broiled chicken, and slice into bite-size pieces.
+
+Slice the baguette in half the long way, and pull the center of the bread out of the baguette halves, leaving a cavity for the filling. Place the baguette halves under the broiler to lightly toast, 2 to 3 minutes.
+
+To assemble the bahn mi sandwich, spread each half of the toasted baguette with mayonnaise, and fill the cavity of the bottom half of the bread with broiled chicken, cucumber slices, pickled carrot, onion, and radish, cilantro leaves, and jalapeno pepper. Squeeze a wedge of lime over the filling, and top with the other half of the baguette.", category_id: 3, user_id: 2, video: "https://www.youtube.com/embed/k4HA1ejw_hA")
+ParentRecipe.create!(title: "Tom Yum Soup", ingredients: "2 liters of water
+4 stalks of lemongrass
+1 inch chunk of galangal
+10 kaffir lime leaves
+10 Thai chilies
+5 cloves of garlic
+½ kilo shrimp
+300 grams of oyster mushrooms
+2 roma tomatoes
+2 white onions (medium sized)
+2 teaspoons of sugar
+8 - 12 tablespoons of fish sauce (depending on your taste)
+8 - 12 tablespoons of lime juice (10 - 15 limes)
+Handful of cilantro
+", 
+directions: "First thing to do is put about 2 liters of water in a pot to boil.
+Then I like to start by squeezing my limes. This is not the first step of the recipe, but it’s best to have your limes squeezed so when you need them later, you don’t need to rush to squeeze them all. Slice about 10 limes and juice them all into a small bowl, and set them aside for later.
+Take your stalks of lemongrass, and first tear off the outermost leaf and throw it out. Then, I like to use a mallet to lightly pound the lemongrass to release the flavors. Then just slice it diagonally into 1 inch strips or so.
+Take about 1 thumb sized chunk of the root part of galangal, and chop it into slices.
+Coarsely break about 10 kaffir lime leaves - no need to cut them, just tear them - which is going to help release their flavor.
+Peel about 5 cloves of garlic.
+I used about 10 Thai bird chilies (prik kee noo) for this recipe, but you can use however many you’d like. First, take off the stem, and then you can either just slice them in two pieces, or give them a little pound on your cutting board like I did (just be careful of flying seeds). You can also remove the seeds if you’d still like the chili flavor but not as much heat.
+Throw the lemongrass, galangal, kaffir lime leaves, garlic and chilies into the water. You can put on the lid just so it starts to boil and release all the herb flavors quicker.
+Now prepare your shrimp (or whatever meat of choice you’re making tom yum with). Since I used goong mae nam (กุ้งแม่น้ำ), freshwater shrimp, I first chopped off all the tentacles.
+Boil your soup with all the herbs in it for about 10 minutes, and you can go ahead and toss in the shrimp and turn the fire to low.
+Then add your mushrooms, which you should pre rinse beforehand.
+Take two roma tomatoes and two small white onions and slice them into wedges - you want them to be big and chunky. Then go ahead and add them to the tom yum.
+If you’re using shrimp for this tom yum recipe, you might get some foam build up on top of your soup, in which case you can just lightly skim it off the top and toss it out.
+Boil the tom yum for about 2 - 3 minutes.
+Next, add about 6 tablespoons of fish sauce first, and 2 teaspoons of sugar. You may need more of each, but start with that.
+Boil your tom yum for another minute or so and then turn off the heat. You don’t want your tom yum to be overcooked. The mushrooms and onions should be soft, and then it’s ready.
+Once your heat is off, go back to your lime juice and add about 8 tablespoons of lime juice (depending on how sour you like it, I like it really sour).
+Make sure you taste test until your tom yum is perfectly sour and salty. You might need to add more fish sauce, sugar, or lime juice.
+Finally chop up a handful of fresh cilantro, throw it in the soup, and give your tom yum a final stir. The lime juice and cilantro taste fresher and more vibrant when not boiled, so that’s why I turn off the heat before adding them.
+You now have Tom yum goong nam sai (ต้มยำกุ้งน้ำใส), the clear version of tom yum.", 
+category_id: 2, 
+user_id: 1, 
+video: "https://www.youtube.com/embed/hXaaZiMgvgI")
+ParentRecipe.create!(title: "Bahn Mi", ingredients: "½ cup rice vinegar
+¼ cup water
+¼ cup white sugar
+¼ cup carrot, cut into 1/16-inch-thick matchsticks
+¼ cup white (daikon) radish, cut into 1/16-inch-thick matchsticks
+¼ cup thinly sliced white onion
+1 skinless, boneless chicken breast half  
+garlic salt to taste
+ground black pepper to taste
+1 (12 inch) French baguette
+4 tablespoons mayonnaise
+¼ cup thinly sliced cucumber
+1 tablespoon fresh cilantro leaves
+1 small jalapeno pepper - seeded and cut into 1/16-inch-thick matchsticks
+1 wedge lime", 
+directions: "Make Pho!", category_id: 2, user_id: 2, video: )
+ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 1, user_id: 3, video: )
+ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 3, user_id: 2, video: )
+ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 4, user_id: 1, video: )
+ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 5, user_id: 2, video: )
+ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 6, user_id: 3, video: )
+ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 7, user_id: 2, video: )
+ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 8, user_id: 1, video: )
+ParentRecipe.create!(title: "Pho", ingredients: "Pho ingredients (broth), meat, veggies", directions: "Make Pho!", category_id: 9, user_id: 2, video: )
 
 UserRecipe.create!(description: "Updating Carbonara the OG way", new_ingredients: "Guanciale", parent_recipe_id: 1, user_id: 2, vote: 35)
 UserRecipe.create!(description: "Another take on Carbonara", new_ingredients: "Bacon", parent_recipe_id: 1, user_id: 1, vote: 16)
 UserRecipe.create!(description: "New take on Pad Thai", new_ingredients: "Paprika", parent_recipe_id: 2, user_id: 1, vote: 20)
 UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
+UserRecipe.create!(description: "Interesting twist on Pho", new_ingredients: "Pineapple", parent_recipe_id: 3, user_id: 2, vote: 4)
 
 Category.create!(name: "Italian")
 Category.create!(name: "Thai")
 Category.create!(name: "Vietnamese")
+Category.create!(name: "French")
+Category.create!(name: "Japanese")
+Category.create!(name: "Indian")
+Category.create!(name: "Greek")
+Category.create!(name: "Peruvian")
+Category.create!(name: "Mexican")
